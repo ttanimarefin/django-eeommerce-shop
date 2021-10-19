237748971a1c58de6d6c153506f1f21ac8f7aa34
ttanimarefin <ttanimarefin@gmail.com>
2021-10-19 15:10:50 +0600

    Register Product model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Sub_Category,Product
+
+
+
+admin.site.register(Category)
+admin.site.register(Sub_Category)
+admin.site.register(Product)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,18 @@ from django.db import models
 class Category(models.Model):
     name= models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Sub_Category(models.Model):
      name=models.CharField(max_length=150)
      category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    
+    
+
+
+class Product(models.Model):
+    image= models.ImageField(upload_to='ecommerce/pimg')
+    name=models.CharField(max_length=100)
+    price=models.IntegerField
+    date=models.DateField(auto_now_add=True)
