5ab16a7b9d5ecf5d79742bcf17a882bb86b54a81
ttanimarefin <ttanimarefin@gmail.com>
2021-10-19 13:36:28 +0600

    Category And Subcategories Model In Project

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name= models.CharField(max_length=150)
+
+class Sub_Category(models.Model):
+     name=models.CharField(max_length=150)
+     category = models.ForeignKey(Category,on_delete=models.CASCADE)
