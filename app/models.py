843645dcda3bea5e141a25ae86028edc758d1b35
ttanimarefin <ttanimarefin@gmail.com>
2021-10-19 22:23:05 +0600

    Admin.py In Register Category and Subcategory Models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from os import name
 from django.db import models
+import sys
 
 # Create your models here.
 class Category(models.Model):
@@ -9,9 +10,12 @@ class Category(models.Model):
         return self.name
 
 class Sub_Category(models.Model):
-     name=models.CharField(max_length=150)
+     name = models.CharField(max_length=264)
      category = models.ForeignKey(Category,on_delete=models.CASCADE)
     
+    
+     def __str__(self):
+        return self.name
     
 
 
