d9c61b37d3ea573af945df83696667c603f4951b
ttanimarefin <ttanimarefin@gmail.com>
2021-10-19 14:50:49 +0600

    Categor And Subcategory Fatch To the database

--- shope/views.py
+++ shope/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from app.models import Category
+
 def Master(request):
     return render(request, 'master.html')
 
 def Index(request):
-    return render(request,'index.html')
+    category=Category.objects.all()
+
+    context={
+        'category':category,
+    }
+    return render(request,'index.html',context)
