e5c63319c17083214de292b8f345f27aefd1b0b9
ttanimarefin <ttanimarefin@gmail.com>
2021-10-20 22:34:44 +0600

    create Signup Page with Django Form

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from os import name
 from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 import sys
+
+from django.db.models import fields
 
 # Create your models here.
 class Category(models.Model):
@@ -28,3 +33,30 @@ class Product(models.Model):
     date=models.DateField(auto_now_add=True)
     def __str__(self):
         return self.name
+
+
+class UserCreateForm(UserCreationForm):
+    email=forms.EmailField(required=True,label='Email',error_messages={'exists':'This Already Exists'})
+
+    class Meta:
+        model=User
+        fields=('username','email','password','password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+
+        def save(self, commit=True):
+            user=super(UserCreateForm,self).save(commit=False)
+            user.email=self.cleaned_data['email']
+            if commit:
+                user.save()
+            return user
+        def clean_email(self):
+            if User.objects.filter(email=self.cleaned_data['email']).exists():
+                raise forms.ValidationError(self.fields['email'].error_message['exists'])
+            return self.cleaned_data['email']
--- shope/views.py
+++ shope/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from app.models import Category,Product, Sub_Category
 
 from app.models import Category
+from django.contrib.auth import authenticate,login
+
+from app.models import UserCreateForm
+
 
 def Master(request):
     return render(request, 'master.html')
@@ -21,3 +25,27 @@ def Index(request):
         
     }
     return render(request,'index.html',context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form =UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user=form.save()
+            new_user= authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            login(request,new_user)
+            return redirect('index')
+    else:
+        form= UserCreateForm()
+
+
+
+        context={
+            'form':form,
+        }
+        
+    
+    return render(request,'registration/signup.html',context)
